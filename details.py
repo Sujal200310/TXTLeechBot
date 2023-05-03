a543803f67534316e63b0400863b01fc09009e0c
Sujal200310 <132435417+Sujal200310@users.noreply.github.com>
2023-05-03 13:09:01 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 2363700
-api_hash = "527cf5174eigfkggigig3611bc69d7b7709"
-bot_token = "6180767554gjggkgkgifBsrDmKItUewgdKGAtGo"
+api_id = 17254521
+api_hash = "753a935500d7c400487c96df5493929c"
+bot_token = "6039771463:AAEM45jcMyZIoqClUkYzlh4KWeASJHgBMns"
 
